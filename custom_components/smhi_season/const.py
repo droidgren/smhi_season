907e698000b7dc1b8052556121e2565722d90537
Anders <adahlgren@gmail.com>
2026-01-01 14:34:41 +0100

    Add constant for current season override
    Added a configuration constant for manual season override.

--- custom_components/smhi_season/const.py
+++ custom_components/smhi_season/const.py
@@ -9,6 +9,9 @@ CONF_HISTORY_SUMMER = "history_summer"
 CONF_HISTORY_AUTUMN = "history_autumn"
 CONF_HISTORY_WINTER = "history_winter"
 
+# Configuration constant for manual season override
+CONF_CURRENT_SEASON = "current_season"
+
 # Seasons
 SEASON_WINTER = "Vinter"
 SEASON_SPRING = "Vår"
